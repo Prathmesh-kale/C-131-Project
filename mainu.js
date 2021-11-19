8923e0388f1dadbd7724ed9253a754bdee06543e
Prathmesh-kale <79739726+Prathmesh-kale@users.noreply.github.com>
2021-11-19 11:47:32 +0530

    Add files via upload

---
+++ mainu.js
@@ -0,0 +1,15 @@
+function bedroom()
+{
+    window.location = "bedroom.html";
+}
+
+function tvac()
+{
+    window.location = "tvac.html";
+}
+
+function desk()
+{
+    window.location = "desk.html";
+}
+
